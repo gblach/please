b76b365f15acf27d9d62ff686271f6f7dfc3ddf6
root <root@deviant.(none)>
2012-07-20 15:58:41 +0200

    Fix checking empty passwords

--- pam.c
+++ pam.c
@@ -4,17 +4,29 @@
 #include <unistd.h>
 #include "pam.h"
 
+#ifndef MAXHOSTNAMELEN
+#define MAXHOSTNAMELEN 256
+#endif
+
 
 int check_password()
 {
     struct pam_conv pamc;
     pam_handle_t *pamh = NULL;
     int retval;
+    char hostname[MAXHOSTNAMELEN];
+    const char *tty;
 
     pamc.conv = &converse;
     pamc.appdata_ptr = NULL;
 
-    retval = pam_start("please", getlogin(), &pamc, &pamh);
+    gethostname(hostname, sizeof(hostname));
+    tty = ttyname(STDERR_FILENO);
+
+    pam_start("please", getlogin(), &pamc, &pamh);
+    pam_set_item(pamh, PAM_RHOST, hostname);
+    pam_set_item(pamh, PAM_RUSER, getlogin());
+    pam_set_item(pamh, PAM_TTY, tty);
     retval = pam_authenticate(pamh, 0);
 
     return retval == PAM_SUCCESS;
@@ -32,35 +44,35 @@ int converse(int n, const struct pam_message **msg, struct pam_response **resp, 
     for(int i = 0; i < n; i++)
     {
         aresp[i].resp_retcode = 0;
-	aresp[i].resp = NULL;
-	
+        aresp[i].resp = NULL;
+
         switch (msg[i]->msg_style)
         {
-	    case PAM_PROMPT_ECHO_OFF:
-	        aresp[i].resp = strdup(getpass(msg[i]->msg));
-		if(aresp[i].resp == NULL) goto fail;
-		break;
-	    
+            case PAM_PROMPT_ECHO_OFF:
+                aresp[i].resp = strdup(getpass(msg[i]->msg));
+                if(aresp[i].resp == NULL) goto fail;
+                break;
+
             case PAM_PROMPT_ECHO_ON:
-		fputs(msg[i]->msg, stderr);
-		if(fgets(buf, sizeof buf, stdin) == NULL) goto fail;
-		aresp[i].resp = strdup(buf);
-		if(aresp[i].resp == NULL) goto fail;
-		break;
-	
+                fputs(msg[i]->msg, stderr);
+                if(fgets(buf, sizeof buf, stdin) == NULL) goto fail;
+                aresp[i].resp = strdup(buf);
+                if(aresp[i].resp == NULL) goto fail;
+                break;
+
             case PAM_ERROR_MSG:
-		fputs(msg[i]->msg, stderr);
-		if(strlen(msg[i]->msg) > 0 && msg[i]->msg[strlen(msg[i]->msg) - 1] != '\n') fputc('\n', stderr);
-		break;
-	
+                fputs(msg[i]->msg, stderr);
+                if(strlen(msg[i]->msg) > 0 && msg[i]->msg[strlen(msg[i]->msg) - 1] != '\n') fputc('\n', stderr);
+                break;
+
             case PAM_TEXT_INFO:
-		fputs(msg[i]->msg, stdout);
-		if (strlen(msg[i]->msg) > 0 && msg[i]->msg[strlen(msg[i]->msg) - 1] != '\n') fputc('\n', stdout);
-		break;
-	    
+                fputs(msg[i]->msg, stdout);
+                if (strlen(msg[i]->msg) > 0 && msg[i]->msg[strlen(msg[i]->msg) - 1] != '\n') fputc('\n', stdout);
+                break;
+
             default:
-		goto fail;
-	}
+                goto fail;
+        }
     }
 
     *resp = aresp;

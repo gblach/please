bcce38799fa6594b4a9695649f6e77ebbeca8bee
Grzegorz Blach <grzegorz@blach.pl>
2015-02-02 04:01:35 +0100

    Remove unneeded pam_setcred(), pam_open_session() and pam_close_session()

--- please.c
+++ please.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 Grzegorz Blach. All rights reserved.
+ * Copyright (c) 2012-2015 Grzegorz Blach. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -64,18 +64,6 @@ int authenticate()
     if(pam_err == PAM_NEW_AUTHTOK_REQD) {
         pam_err = pam_chauthtok(pamh, PAM_CHANGE_EXPIRED_AUTHTOK);
     }
-    PAM_RETURN_ON_FAILURE;
-
-    pam_err = pam_setcred(pamh, PAM_ESTABLISH_CRED);
-    PAM_RETURN_ON_FAILURE;
-
-    pam_err = pam_open_session(pamh, 0);
-    PAM_RETURN_ON_FAILURE;
-
-    pam_err = pam_close_session(pamh, 0);
-    PAM_RETURN_ON_FAILURE;
-
-    pam_err = pam_setcred(pamh, PAM_DELETE_CRED);
     PAM_RETURN_ON_FAILURE;
 
 pam_return:
